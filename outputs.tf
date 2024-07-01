47bd104471185ae2f4530cfadd1ee17df0ff791a
Antonio Berbece <razvan.berbece@hotmail.com>
2024-07-01 03:17:28 +0100

    fix failing output (2)

--- outputs.tf
+++ outputs.tf
@@ -7,8 +7,3 @@ output "github_repositories_html_urls" {
   value       = module.repositories.github_repositories_html_urls
   description = "URLs of the GitHub repositories managed by this resource"
 }
-
-output "collaborator_invitation_ids" {
-  value       = module.repositories.collaborator_invitation_ids
-  description = "Invitation IDs of the collaborators added to the GitHub repositories managed by this resource"
-}
